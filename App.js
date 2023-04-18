59c4822fbbd53a9cb5bccd79785531709d686ef6
Bytefield <ramosperez1978@gmail.com>
2023-04-18 17:32:58 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import PatientListScreen from './src/screens/PatientListScreen';
+import PatientDetailScreen from './src/screens/PatientDetailScreen';
+import CreateAppointmentScreen from './src/screens/CreateAppointmentScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Register">
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ title: 'Login' }}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterScreen}
+                    options={{ title: 'Register' }}
+                />
+                <Stack.Screen
+                    name="PatientList"
+                    component={PatientListScreen}
+                    options={{ title: 'Patient List' }}
+                />
+                <Stack.Screen
+                    name="PatientDetail"
+                    component={PatientDetailScreen}
+                    options={{ title: 'Patient Detail' }}
+                />
+                <Stack.Screen
+                    name="CreateAppointment"
+                    component={CreateAppointmentScreen}
+                    options={{ title: 'Create Appointment' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
